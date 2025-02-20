7c02b9923a14384caeccd4c62c7591f99c96c46d
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2025-02-20 17:11:44 +0500

    create first test1 file py

---
+++ test/test1.py
@@ -0,0 +1,7 @@
+first = input("Введите имя: ")
+second = input("Введите фамилию: ")
+third = input("Введите отчество: ")
+
+initials = second + " "+ first[0] + " " + third[0]
+
+print("Ваши инициалы: ", initials)
