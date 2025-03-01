e07e4e5977e1166c42a9042f51a0b4c1ccf43a72
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2025-03-01 20:10:35 +0500

    create simple docker compose file

--- test/test1.py
+++ test/test1.py
@@ -1,7 +1,10 @@
-first = input("Введите имя: ")
-second = input("Введите фамилию: ")
-third = input("Введите отчество: ")
+# first = input("Введите имя: ")
+# second = input("Введите фамилию: ")
+# third = input("Введите отчество: ")
 
-initials = second + " "+ first[0] + " " + third[0]
+# initials = second + " "+ first[0] + " " + third[0]
 
-print("Ваши инициалы: ", initials)
+# print("Ваши инициалы: ", initials)
+print("Желаем вам приятного времяприпровождения!")
+name = "Khammerson"
+print(f"{name:<10}")
