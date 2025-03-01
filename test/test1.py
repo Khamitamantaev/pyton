ce2342b9d7c49d5120af85b83031caf17aca5d71
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2025-03-01 21:29:56 +0500

    add django project first

--- test/test1.py
+++ test/test1.py
@@ -1,10 +1,2 @@
-# first = input("Введите имя: ")
-# second = input("Введите фамилию: ")
-# third = input("Введите отчество: ")
-
-# initials = second + " "+ first[0] + " " + third[0]
-
-# print("Ваши инициалы: ", initials)
-print("Желаем вам приятного времяприпровождения!")
-name = "Khammerson"
-print(f"{name:<10}")
+import django
+print(django.get_version())
