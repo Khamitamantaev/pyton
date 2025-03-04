5c18e582c87377594aae3b374f0167cc9ad69982
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2025-03-04 20:17:15 +0500

    with render result

--- django/khammerson/views.py
+++ django/khammerson/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello my dear friend. This is a simple Khammerson project!")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
     return  HttpResponse("You are looking at question %s." % question_id)
@@ -16,3 +20,4 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You are voting on question %s." % question_id)
+
