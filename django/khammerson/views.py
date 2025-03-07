976c59dab86f63e46655fb844a02b0fabe3dbc83
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2025-03-08 00:06:39 +0500

    add simple dockerize django app

--- django/khammerson/views.py
+++ django/khammerson/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import loader
 from .models import Question
 
@@ -9,10 +9,11 @@ from .models import Question
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+    return render(request, "khammerson/index.html", context)
 
 def detail(request, question_id):
-    return  HttpResponse("You are looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "khammerson/detail.html", {"question": question})
 
 def results(request, question_id):
     response = "You are looking at the results of question %s."
